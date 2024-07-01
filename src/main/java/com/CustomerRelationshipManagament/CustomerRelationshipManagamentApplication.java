b52d6166096f098d60c5eef252dfa33c83cf6d03
AmoghParulekar2003 <amoghmparulekar@gmail.com>
2024-07-01 20:55:32 +0530

    Java Project

---
+++ src/main/java/com/CustomerRelationshipManagament/CustomerRelationshipManagamentApplication.java
@@ -0,0 +1,13 @@
+package com.CustomerRelationshipManagament;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerRelationshipManagamentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerRelationshipManagamentApplication.class, args);
+	}
+
+}
